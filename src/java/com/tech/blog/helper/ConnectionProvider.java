eec6cb496345aa81a12d3e522c48ae9d7dd87ffc
Sanidhya <sharmasanidhya0504@gmail.com>
2023-01-11 23:57:03 +0530

    connected to mysql

---
+++ src/java/com/tech/blog/helper/ConnectionProvider.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.tech.blog.helper;
+import java.sql.*;
+public class ConnectionProvider {
+    private static Connection con;
+    public static Connection getConnection(){
+        System.out.println("hello");
+        try{
+                if(con==null){
+                    //driver class load
+               
+                    Class.forName("com.mysql.jdbc.Driver");
+                    //create a connection
+                    con=DriverManager.
+                            getConnection("jdbc:mysql://localhost:3306/techblog","user1","admin");
+
+                }
+
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+        return con;
+    }
+}
