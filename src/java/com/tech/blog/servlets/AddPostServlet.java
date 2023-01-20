734833d00a6872dc55cdb8aebd8bd8651b932b71
Sanidhya <sharmasanidhya0504@gmail.com>
2023-01-20 10:06:37 +0530

    created PostDao and post entity

---
+++ src/java/com/tech/blog/servlets/AddPostServlet.java
@@ -0,0 +1,121 @@
+
+package com.tech.blog.servlets;
+
+import com.tech.blog.dao.PostDao;
+import com.tech.blog.entities.Post;
+import com.tech.blog.entities.User;
+import com.tech.blog.helper.ConnectionProvider;
+import com.tech.blog.helper.Helper;
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.MultipartConfig;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import javax.servlet.http.Part;
+
+/**
+ *
+ * @author Durgesh
+ */
+@MultipartConfig
+public class AddPostServlet extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    private static String getSubmittedFileName(Part part) {
+    for (String cd : part.getHeader("content-disposition").split(";")) {
+        if (cd.trim().startsWith("filename")) {
+            String fileName = cd.substring(cd.indexOf('=') + 1).trim().replace("\"", "");
+            return fileName.substring(fileName.lastIndexOf('/') + 1).substring(fileName.lastIndexOf('\\') + 1); // MSIE fix.
+        }
+    }
+    return null;
+}
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+
+            int cid = Integer.parseInt(request.getParameter("cid"));
+            String pTitle = request.getParameter("pTitle");
+            String pContent = request.getParameter("pContent");
+            String pCode = request.getParameter("pCode");
+            Part part = request.getPart("pic");
+//            getting currentuser id
+            //HttpSession session = request.getSession();
+            HttpSession session = request.getSession();
+            User u =(User)  session.getAttribute("currentUser");
+            
+//            out.println("your post title is " + pTitle);
+  //     out.println(part.getSubmittedFileName());
+       String img=part.getSubmittedFileName();
+       //out.println(u.getId());
+            Post p = new Post(pTitle, pContent, pCode, img, null, cid, u.getId());
+            PostDao dao = new PostDao(ConnectionProvider.getConnection());
+            if (dao.savePost(p)) {
+                String path = request.getRealPath("/") + "blog" + File.separator + getSubmittedFileName(part);
+                //out.println(path);
+              //  Helper.saveFile(part.getInputStream(), path);
+              if(Helper.saveFile(part.getInputStream(), path))
+                out.println("done");
+              else
+                  out.println("not");
+            } else {
+                out.println("error");
+            }
+
+        }
+    }
+
+// <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
